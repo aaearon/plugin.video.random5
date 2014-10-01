1780b122d2a08336c05c45e7637d185619788e6f
aaearon <aaearon@gmail.com>
2014-10-01 16:37:47 +0200

    works as intended with this commit. now need to add some logging and comments as well as settings

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 random5
 =======
 
-Plays 5 random episodes from a chosen TV show or entire library. Easy way to decide what to watch when falling asleep.
+A simple XBMC plugin that plays 5 random episodes from a chosen TV show or entire library. Easy way to decide what to watch when falling asleep.
--- addon.py
+++ addon.py
@@ -1,36 +1,36 @@
 import sys
 import xbmc
-import xbmcaddon
 import xbmcgui
 import xbmcplugin
 import random
+import urlparse
 
 from resources.lib.xbmcjson import XBMC as xbmcjson
 
-__addon__ = xbmcaddon.Addon(id='plugin.video.random5')
-__addonname__ = __addon__.getAddonInfo('name')
-__icon__ = __addon__.getAddonInfo('icon')
-
+args = urlparse.parse_qs(sys.argv[2][1:])
 addon_handle = int(sys.argv[1])
 
 x = xbmcjson("http://localhost/jsonrpc")
 
+path = args.get('path', None)
+
 def getShows():
     shows = []
 
-    request = x.VideoLibrary.GetTVShows()['result']['tvshows']
+    request = x.VideoLibrary.GetTVShows()
 
-    for s in request:
+    for s in request['result']['tvshows']:
         shows.append(s)
 
     return shows
 
 def getEpisodes(show):
+    show = int(show) # Needs to be an integer
     episodes = []
 
-    request = x.VideoLibrary.GetEpisodes({"tvshowid": show, "properties": ["title", "file"]})['result']['episodes']
+    request = x.VideoLibrary.GetEpisodes({"tvshowid": show, "properties": ["title", "file"]})
 
-    for e in request:
+    for e in request['result']['episodes']:
         episodes.append(e)
 
     return episodes
@@ -42,7 +42,7 @@ def getRandomEpisodes(number, show):
     episodes = []
     all_episodes = getEpisodes(show)
 
-    #HARDCODED: returns 5 regardless of what number is
+    #HARDCODED: returns 5 regardless of what number is, eventually this will be configurable value
     for i in range(5):
         episodes.append(random.choice(all_episodes))
 
@@ -55,21 +55,28 @@ def createPlaylist(shows):
     for s in shows:
         addItemToPlaylist(s)
 
+def createAndPlay(show):
+    episodes = getRandomEpisodes(5, show)
+    createPlaylist(episodes)
+    x.Player.Open({"item": {"playlistid": 1}})
+
 def createMenu(shows):
     #Create 'entire library' list item
-    li = xbmcgui.ListItem('ALL TV SHOWS', iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url='show', listitem=li, isFolder=False)
+    # li = xbmcgui.ListItem('ALL TV SHOWS', iconImage='DefaultFolder.png')
+    # xbmcplugin.addDirectoryItem(handle=addon_handle, url='show', listitem=li, isFolder=False)
 
     for s in shows:
+        url = sys.argv[0] + "?path=/play&show=%s" % (s['tvshowid'])
         li = xbmcgui.ListItem(s['label'], iconImage='DefaultFolder.png')
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url='show', listitem=li, isFolder=False)
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=False)
 
     xbmcplugin.endOfDirectory(addon_handle)
 
-# createMenu(getShows())
+if path is None:
+    createMenu(getShows())
 
-reps = getRandomEpisodes(10, 1)
-createPlaylist(reps)
-x.Player.Open({"item": {"playlistid": 1}})
+elif path[0] == '/play':
+    showid = args.get('show', None)
+    createAndPlay(showid[0])
 
 
