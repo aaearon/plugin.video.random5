c735411c02884147d636045e99e6616abdd939ea
aaearon <aaearon@gmail.com>
2014-10-01 14:59:14 +0200

    need to hook it all up in the gui now. some methods probably dont even need to exist but i can clean that up later

--- addon.py
+++ addon.py
@@ -3,6 +3,7 @@ import xbmc
 import xbmcaddon
 import xbmcgui
 import xbmcplugin
+import random
 
 from resources.lib.xbmcjson import XBMC as xbmcjson
 
@@ -12,36 +13,63 @@ __icon__ = __addon__.getAddonInfo('icon')
 
 addon_handle = int(sys.argv[1])
 
-title = "Hello World"
-text = "This is some text"
-time = 5000 #ms
-
-xbmc.executebuiltin('Notification(%s, %s, %d, %s)' % (title, text, time, __icon__))
-
 x = xbmcjson("http://localhost/jsonrpc")
-# request = x.VideoLibrary.GetTVShows({"properties":["studio", "genre"]})
-#request = x.VideoLibrary.GetTVShows()['result']['tvshows']
-# request = x.VideoLibrary.GetEpisodes({"tvshowid": 1, "properties": ["title"]})
-# request = x.JSONRPC.Ping()
-#xbmc.log('%s' % request[0])
 
 def getShows():
     shows = []
+
     request = x.VideoLibrary.GetTVShows()['result']['tvshows']
-    # xbmc.log('%s' % request)
+
     for s in request:
         shows.append(s)
-    # xbmc.log('%s' % len(shows))
+
     return shows
 
+def getEpisodes(show):
+    episodes = []
+
+    request = x.VideoLibrary.GetEpisodes({"tvshowid": show, "properties": ["title", "file"]})['result']['episodes']
+
+    for e in request:
+        episodes.append(e)
+
+    return episodes
+
+def addItemToPlaylist(episode):
+    request = x.Playlist.Add({"item": {"file": episode['file']}, "playlistid": 1})
+
+def getRandomEpisodes(number, show):
+    episodes = []
+    all_episodes = getEpisodes(show)
+
+    #HARDCODED: returns 5 regardless of what number is
+    for i in range(5):
+        episodes.append(random.choice(all_episodes))
+
+    return episodes
+
+def createPlaylist(shows):
+    # Clear the playlist before we add items to it
+    request = x.Playlist.Clear({"playlistid": 1})
+
+    for s in shows:
+        addItemToPlaylist(s)
 
 def createMenu(shows):
+    #Create 'entire library' list item
+    li = xbmcgui.ListItem('ALL TV SHOWS', iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url='show', listitem=li, isFolder=False)
+
     for s in shows:
         li = xbmcgui.ListItem(s['label'], iconImage='DefaultFolder.png')
         xbmcplugin.addDirectoryItem(handle=addon_handle, url='show', listitem=li, isFolder=False)
 
     xbmcplugin.endOfDirectory(addon_handle)
 
-createMenu(getShows())
+# createMenu(getShows())
+
+reps = getRandomEpisodes(10, 1)
+createPlaylist(reps)
+x.Player.Open({"item": {"playlistid": 1}})
 
 
